b73bb9ea7e566479fcce92a57e741291219165cd
OleskoG <91556273+OleskoG@users.noreply.github.com>
2021-10-08 18:46:25 +0500

    Task1_TRY1_Golovina
    Additional discription for my teachers ;)

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,21 +1,32 @@
-function calcShipping(sum, min, shipping) {
+function calcShipping(sum, min, shipping, delvry) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+    
+       // Задание №2.1. Рассчитать доставку
+      // создайте переменную shippingSum
+    
+    let shippingSum = delvry; // стоимость доставки конкретного заказа
 
-    // Задание №2.1. Рассчитать доставку
-
-    // создайте переменную shippingSum
-
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
+      // если productsSum равно 0,
+      // то shippingSum присвоить значение 0
+    
+    if (productsSum == 0)
+    {
+        then shippingSum = 0;
+    }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
+           if (productsSum >= freeShippingMinSum)
+           {
+               then shippingSum = 0;
+           }
+    // если productsSum больше 0   и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+                   if (productsSum > 0) && (productsSum < freeShippingMinSum)  
+                    {
+                       then shippingSum = shippingPrice;
+                    }
     // Конец решения задания №2.1.
 
     return shippingSum;
