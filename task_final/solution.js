cb4b8d5b5651878d34d2cdc898e524a4c5a8a8c5
olesko <oep05@mail.ru>
2021-11-16 23:03:38 +0500

    try2

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,53 +1,19 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
-
 function sendRequest(name, phone, address [street, house, entrance, floor, flat], goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: '', order {address: '', sum: ''}, goods: ''}; 
+}
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push ({title: goods[i].title, count: goods[i].count});
+        
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул. ${street}, дом ${house}, подъезд ${entrance}, этаж ${floor}, кв. ${flat}" ;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = '${name} ${phone}';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data: data}); 
 
     return jsonData;
-}
